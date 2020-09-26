a83a813e995d58283a340d2a4476711d2d40a8a3
Gginne <ggamerstar@gmail.com>
2020-09-26 19:22:53 -0400

    configured database connections

---
+++ project/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Store202</title>
+</head>
+<body>
+    <h1>Store202</h1>
+    <button><a href="/register.php">Register</a></button>
+    <button>Login</button>
+</body>
+</html>
+
