4f858490e1d1f5b4b1f979e1988b4a0f9cf86526
Gginne <ggamerstar@gmail.com>
2020-10-13 20:57:20 -0400

    Changed navbar links position

--- project/partials/header.php
+++ project/partials/header.php
@@ -5,41 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Store202</title>
-    
-    <style>
-    body{
-        padding: 0 2rem;
-        font-size: 16px;
-        font-family: sans-serif;
-        color: #333;
-    }
-    nav{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 1rem;
-    }
-
-    nav .brand{
-        font-size: 20px;
-        border: 3px solid #333;
-        padding: 7px;
-    }
-    
-    nav ul{
-        display: flex;
-        list-style: none;
-    }
-
-    nav ul li{
-        margin: .5rem;
-    }
-
-    nav ul li a{
-        text-decoration: none;
-    }
-
-    </style>
+    <link rel="stylesheet" href="static/css/style.css">
 </head>
 
 
--- project/partials/nav.php
+++ project/partials/nav.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
     
 <nav>
   <p class="brand">Store202</p>
-  <ul>
+  <ul class="nav">
     <li><a href="home.php">Home</a></li>
     <?php if(!is_logged_in()):?>
     <li><a href="login.php">Login</a></a></li>
