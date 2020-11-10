4ede94b4282a1feadfb673577ed4194b87e9576a
Gginne <ggamerstar@gmail.com>
2020-11-09 22:34:46 -0500

    Added new admin routes

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -11,8 +11,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
       <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
-      <li><a href="test_create_product.php">Create Egg</a></li>
-      <li><a href="test_list_product.php">View Eggs</a></li>
+      <li><a href="test_create_product.php">Create Products</a></li>
+      <li><a href="test_list_product.php">View Products</a></li>
     <?php endif; ?>
     <?php if(is_logged_in()):?>
       <li><a href="profile.php">Profile</a></li>
