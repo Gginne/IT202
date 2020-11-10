4d2776f0c72338316bfe2fe6c9de4f3c52797489
Gginne <ggamerstar@gmail.com>
2020-11-09 22:31:28 -0500

    Added test list products

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -7,12 +7,16 @@ require_once(__DIR__ . "/../lib/helpers.php");
   <ul class="nav">
     <li><a href="home.php">Home</a></li>
     <?php if(!is_logged_in()):?>
-    <li><a href="login.php">Login</a></a></li>
-    <li><a href="register.php">Register</a></li>
+      <li><a href="login.php">Login</a></a></li>
+      <li><a href="register.php">Register</a></li>
+    <?php endif; ?>
+    <?php if (has_role("Admin")): ?>
+      <li><a href="test_create_product.php">Create Egg</a></li>
+      <li><a href="test_list_product.php">View Eggs</a></li>
     <?php endif; ?>
     <?php if(is_logged_in()):?>
-    <li><a href="profile.php">Profile</a></li>
-    <li><a href="logout.php">Logout</a></li>
+      <li><a href="profile.php">Profile</a></li>
+      <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
   </ul>
 </nav>
