be6974c4b5a55f00d495ab84f419015e4578fa11
Gginne <ggamerstar@gmail.com>
2020-11-12 10:12:35 -0500

    Changed navbar and alerts to bootstrap

--- project/partials/flash.php
+++ project/partials/flash.php
@@ -6,8 +6,8 @@
     <?php $messages = getMessages(); ?>
     <?php if ($messages): ?>
         <?php foreach ($messages as $msg): ?>
-            <div class="row bg-secondary justify-content-center">
-                <p><?php echo $msg; ?></p>
+            <div class="alert alert-primary" role="alert">
+                <?php echo $msg; ?>
             </div>
         <?php endforeach; ?>
     <?php endif; ?>
--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,22 +1,45 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand logo" href="#">Store202</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    
-<nav>
-  <p class="brand">Store202</p>
-  <ul class="nav">
-    <li><a href="<?php echo getURL("home.php");?>">Home</a></li>
-    <?php if(!is_logged_in()):?>
-      <li><a href="<?php echo getURL("login.php");?>">Login</a></a></li>
-      <li><a href="<?php echo getURL("register.php");?>">Register</a></li>
-    <?php endif; ?>
-    <?php if (has_role("Admin")): ?>
-      <li><a href="<?php echo getURL("test/test_create_product.php");?>">Create Products</a></li>
-      <li><a href="<?php echo getURL("test/test_list_product.php");?>">View Products</a></li>
-      <li><a href="<?php echo getURL("test/test_create_cart.php");?>">Create Carts</a></li>
-      <li><a href="<?php echo getURL("test/test_list_cart.php");?>">View Carts</a></li>
-    <?php endif; ?>
-    <?php if(is_logged_in()):?>
-      <li><a href="<?php echo getURL("profile.php");?>">Profile</a></li>
-      <li><a href="<?php echo getURL("logout.php");?>">Logout</a></li>
-    <?php endif; ?>
-  </ul>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo getURL("home.php");?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <?php if(!is_logged_in()):?>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo getURL("login.php");?>">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo getURL("register.php");?>">Register</a>
+      </li>
+      <?php endif; ?>
+      <?php if (has_role("Admin")): ?>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Test
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="<?php echo getURL("test/test_create_product.php");?>">Create Products</a>
+          <a class="dropdown-item" href="<?php echo getURL("test/test_list_product.php");?>">View Products</a>
+          <a class="dropdown-item" href="<?php echo getURL("test/test_create_cart.php");?>">Create Carts</a>
+          <a class="dropdown-item" href="<?php echo getURL("test/test_list_cart.php");?>">View Carts</a>
+          
+        </div>
+      </li>
+      <?php endif; ?>
+      <?php if(is_logged_in()):?>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo getURL("profile.php");?>">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo getURL("logout.php");?>">Logout</a>
+      </li>
+      <?php endif; ?>
+    </ul>
+  </div>
 </nav>
+
