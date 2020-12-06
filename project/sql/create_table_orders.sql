1bb39a9da26aebc7006abdb8f16b8c15c4cf062f
Gginne <ggamerstar@gmail.com>
2020-12-06 12:32:56 -0500

    Added order items table

---
+++ project/sql/create_table_orders.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `Orders`
+(
+    id int auto_increment,
+	product_id int,
+	user_id int,
+	quantity int,
+	unit_price int,
+	orderRef int, -- this will be a manually handled id to group order items together
+    modified    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update current_timestamp,
+    created     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+	primary key (id),
+	foreign key (user_id) references Users(id),
+	foreign key (product_id) references Products(id)
+)
